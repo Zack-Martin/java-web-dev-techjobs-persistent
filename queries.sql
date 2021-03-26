06c08b3b3f63acc107e420f8fe253eaf19cb12a3
Pimpod <zmartin2998@gmail.com>
2021-03-25 20:23:40 -0500

    Added Queries

--- queries.sql
+++ queries.sql
@@ -4,7 +4,8 @@
 -- name varchar
 -- skills varchar
 ## Part 2: Test it with SQL
-
+-- SELECT * FROM employer WHERE location = "St. Louis City";
 ## Part 3: Test it with SQL
-
+-- DROP TABLE job;
 ## Part 4: Test it with SQL
+-- SELECT name, description FROM skill LEFT JOIN job_skills ON skill.id = job_skills.skills_id WHERE job_id IS NOT NULL ORDER BY skill.name
